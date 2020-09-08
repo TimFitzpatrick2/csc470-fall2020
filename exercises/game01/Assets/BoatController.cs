279e3ef080110838822af5d6409a8ba47d7ed57c
TimFitzpatrick2 <tf3819a@student.american.edu>
2020-09-08 15:16:11 -0400

    small assignment

---
+++ exercises/game01/Assets/BoatController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("All aboard: " + gameObject.name);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, 0.5f);
+    }
+}
