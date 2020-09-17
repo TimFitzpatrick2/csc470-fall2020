b8f24574c4f3abc81d69cdf4829cd16f554dcdf7
TimFitzpatrick2 <tf3819a@student.american.edu>
2020-09-16 20:18:35 -0400

    game02

---
+++ exercises/game02/Assets/GameManger.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManger : MonoBehaviour
+{
+    public GameObject WorldPrefab;
+    public GameObject DogBone;
+    GameObject ground;
+
+    float makeboneTime = .06f;
+    float makeboneRate = .06f;
+
+    int numWorldz = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ground = GameObject.Find("Ground");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        makeboneTime -= Time.deltaTime;
+        if (makeboneTime < 0){
+            Vector3 pos = new Vector3(ground.transform.position.x + Random.Range(-200, 200)
+                                , ground.transform.position.y + 100,
+                                ground.transform.position.z + Random.Range(-200, 200));
+            GameObject drop = Instantiate(DogBone, pos, Quaternion.identity);
+            Renderer rend = drop.GetComponent<Renderer>();
+            rend.material.color = new Color(Random.value, Random.value, Random.value);
+            Destroy(drop, 1f);
+
+            makeboneTime = makeboneRate;
+        }
+    }
+
+    public void MakeMoreWorldz()
+    {
+        numWorldz++;
+
+        if (numWorldz < 10)
+        {
+
+
+
+            Debug.Log("Make Worldz");
+            Vector3 pos = new Vector3(ground.transform.position.x + Random.Range(-20, 20)
+                                    , ground.transform.position.y + 50,
+                                    ground.transform.position.z + Random.Range(-20, 20));
+            Instantiate(WorldPrefab, pos, Quaternion.identity);
+        } else {
+            SceneManager.LoadScene("New World");
+        }
+    }
+}
---
+++ exercises/game02/Assets/LouieController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LouieController : MonoBehaviour
+{
+    float speed = 50f;
+    float rotateSpeed = 150f;
+    int score = 0;
+
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        transform.Rotate(0, hAxis * rotateSpeed * Time.deltaTime, 0, Space.World);
+
+        transform.Translate(transform.forward * Time.deltaTime * speed, Space.World);
+
+        if (speed > 0)
+        {
+            speed -= 4 * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            speed += 100;
+        }
+
+        speed = Mathf.Clamp(speed, 0, 100);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Worldz"))
+            
+            Destroy(other.gameObject);
+            score++;
+            scoreText.text = score.ToString();
+
+    }
+}
