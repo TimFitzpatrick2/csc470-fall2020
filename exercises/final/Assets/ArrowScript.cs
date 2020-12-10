c9c14a6173d0bbe04cbe44064435e258ffbbe67a
TimFitzpatrick2 <tf3819a@student.american.edu>
2020-12-10 13:27:35 -0500

    final

--- exercises/final/Assets/ArrowScript.cs
+++ exercises/final/Assets/ArrowScript.cs
@@ -32,9 +32,9 @@ public class ArrowScript : MonoBehaviour
         if (other.CompareTag("Target"))
         {
             Destroy(other.gameObject);
-            //audioSource.clip = clips[audioIndex % clips.Length];
-            //audioIndex++;
-            //audioSource.Play();
+            audioSource.clip = clips[audioIndex % clips.Length];
+            audioIndex++;
+            audioSource.Play();
 
             score--;
 
